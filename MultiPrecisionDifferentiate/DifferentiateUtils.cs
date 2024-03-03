e3caeaa0db930193913a4772b9fcf442dfa0e1dc
tk-yoshimura <yoshimura8192@gmail.com>
2024-03-03 16:21:06 +0900

    add: utils estimate precision

---
+++ MultiPrecisionDifferentiate/DifferentiateUtils.cs
@@ -0,0 +1,53 @@
+﻿using MultiPrecision;
+
+namespace MultiPrecisionDifferentiate {
+    public static class DifferentiateUtils {
+        public static (MultiPrecision<N> h, MultiPrecision<N> value, long actual_bits) EstimatePrecision<N>(
+            IEnumerable<(MultiPrecision<N> h, MultiPrecision<N> value)> values) where N : struct, IConstant {
+
+            if (values.Count() <= 1 || values.Select(v => v.h).Distinct().Count() != values.Count()) {
+                throw new ArgumentException("array require multiple and unique 'h'.", nameof(values));
+            }
+
+            static long match_bits2(MultiPrecision<N> v1, MultiPrecision<N> v2) {
+                if (!MultiPrecision<N>.IsFinite(v1) || !MultiPrecision<N>.IsFinite(v2)) {
+                    return 0;
+                }
+
+                MultiPrecision<N> dv = v1 - v2;
+
+                long bits = long.Clamp(long.Max(v1.Exponent, v2.Exponent) - dv.Exponent, 0, MultiPrecision<N>.Bits);
+
+                return bits;
+            }
+
+            static long match_bits3(MultiPrecision<N> v1, MultiPrecision<N> v2, MultiPrecision<N> v3) {
+                return long.Min(match_bits2(v1, v2), match_bits2(v2, v3));
+            }
+
+            List<(MultiPrecision<N> h, MultiPrecision<N> value)> values_sorted = [.. values.OrderByDescending(v => v.h)];
+
+            if (values.Count() == 2) {
+                return (
+                    values_sorted[1].h,
+                    values_sorted[1].value,
+                    match_bits2(values_sorted[0].value, values_sorted[1].value)
+                );
+            }
+
+            MultiPrecision<N> best_h = MultiPrecision<N>.NaN, best_value = MultiPrecision<N>.NaN;
+            long max_match_bits = 0;
+
+            for (int i = 1; i < values_sorted.Count - 1; i++) {
+                long match_bits = match_bits3(values_sorted[i - 1].value, values_sorted[i].value, values_sorted[i + 1].value);
+
+                if (match_bits > max_match_bits) {
+                    (best_h, best_value) = values_sorted[i];
+                    max_match_bits = match_bits;
+                }
+            }
+
+            return (best_h, best_value, max_match_bits);
+        }
+    }
+}
---
+++ MultiPrecisionDifferentiateTest/DifferentiateUtilsTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiPrecision;
+using MultiPrecisionDifferentiate;
+
+namespace MultiPrecisionDifferentiateTest {
+    [TestClass()]
+    public class DifferentiateUtilsTests {
+        [TestMethod()]
+        public void EstimatePrecisionTest() {
+            List<(MultiPrecision<Pow2.N16> h, MultiPrecision<Pow2.N16> value)> values = [];
+
+            for (int exponent = -32; exponent <= -16; exponent++) {
+                MultiPrecision<Pow2.N16> h = MultiPrecision<Pow2.N16>.Ldexp(1, exponent);
+                MultiPrecision<Pow2.N16> value = MultiPrecision<Pow2.N16>.PI + MultiPrecision<Pow2.N16>.Square(exponent + 24) * 1e-20;
+
+                values.Add((h, value));
+
+                Console.WriteLine($"{exponent},{value}");
+            }
+
+            (MultiPrecision<Pow2.N16> best_h, MultiPrecision<Pow2.N16> best_value, long actual_bits) =
+                DifferentiateUtils.EstimatePrecision(values);
+
+            int actual_digits = (int)(actual_bits * 0.30103);
+
+            Console.WriteLine("best");
+            Console.WriteLine(best_h.Exponent);
+            Console.WriteLine(best_value);
+            Console.WriteLine(best_value.ToString($"e{actual_digits}"));
+            Console.WriteLine(actual_digits);
+            Console.WriteLine(actual_bits);
+
+            Assert.AreEqual(-24, best_h.Exponent);
+            Assert.AreEqual(20, actual_digits);
+        }
+    }
+}
